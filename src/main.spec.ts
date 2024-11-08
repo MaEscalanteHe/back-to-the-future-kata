e2c1f5dc78a445c046984ec766633661398401af
Jose Barrera <jose@acidtango.com>
2024-11-08 10:37:18 -0500

    red: should add apple

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,14 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { Inventory } from "./main.js"
 
 describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
-
-    expect(result).toBe(true)
+  const yesterday = new Date("2024-11-14")
+  const today = new Date("2024-11-15")
+  const tomorrow = new Date("2024-11-16")
+  const future = new Date("2025-01-01")
+  it("should add apple", () => {
+    const inventory = new Inventory()
+    inventory.add(yesterday, "apple")
+    expect(inventory.project(yesterday)).toBe(["apple"])
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
-export function myFunction() {
-  return true
+export class Inventory {
+  constructor(private storage: Map<Date, string[]> = new Map()) {}
+
+  add(date: Date, product: string) {
+    throw new Error("Not implemented")
+  }
+
+  project(date: Date) {
+    throw new Error("Not implemented")
+  }
 }
+
+const yesterday = new Date("2024-11-14")
+const today = new Date("2024-11-15")
+const tomorrow = new Date("2024-11-16")
+const future = new Date("2025-01-01")
