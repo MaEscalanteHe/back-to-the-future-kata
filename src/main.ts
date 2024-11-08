8adfd432e259b000e26929ecb060f0e050b27bd2
Manuel Escalante <maescalantehe@gmail.com>
2024-11-08 12:43:36 -0400

    green: should remove apple from today

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,30 @@ export class Inventory {
 
   project(date: Date) {
     const inventory = [...this.storage.entries()].sort(([a], [b]) => a.getTime() - b.getTime())
-    const result = inventory.reduce((acc: any, [d, products]) => {
+    const inventoryFlated = inventory.reduce<string[]>((acc: any, [d, products]) => {
       date >= d && acc.push(...products)
       return acc
     }, [])
-    return result
+
+    let elementWithoutTilde = ""
+    inventoryFlated.forEach((element, index) => {
+      if (element.includes("~")) {
+        elementWithoutTilde = element.replace("~", "")
+        inventoryFlated.splice(index, 1)
+      }
+    })
+    if (elementWithoutTilde !== "") {
+      inventoryFlated.splice(
+        inventoryFlated.findIndex((e) => e === elementWithoutTilde),
+        1,
+      )
+    }
+
+    return inventoryFlated
   }
 
-  remove(date, product: string) {
-    return
+  remove(date: Date, product: string) {
+    const products = this.storage.get(date) || []
+    this.storage.set(date, [...products, `~${product}`])
   }
 }
